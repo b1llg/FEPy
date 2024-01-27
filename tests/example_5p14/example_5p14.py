b7f5e9e4a7a013738f6f1278097ac798cc9b2e93
b1llg <guy.billy@outlook.com>
2024-01-27 18:44:49 -0500

    add lambda for loads

--- tests/example_5p14/example_5p14.py
+++ tests/example_5p14/example_5p14.py
@@ -37,10 +37,9 @@ def main():
     gamma_l = LoadBc("load_point", [-150])
 
     # Define distributed load on line #1
-    def pload(x):
-        return -1*(6+40*x)
+    pload = lambda x : -1*(6+40*x)
     
-    gamma_dl1 = LoadBc("loaded_section_ramp", [load_function])
+    gamma_dl1 = LoadBc("loaded_section_ramp", [pload])
 
     # Define constant load on other elements
     gamma_dl2 = LoadBc("loaded_section_const", [6])
@@ -49,14 +48,14 @@ def main():
     tension = 400 # N
 
     # Generate bilinear form from the weak form
-    a = lambda  u, w, x : pload(x)*u*w + tension*grad(u)*grad(w)
-    l = lambda r : r(x)
+    a = lambda  u, w :  tension*grad(u)*grad(w)
+    l = lambda pload : pload*w
 
     # read model data and assign displacement field
     path = os.path.join(os.path.dirname(__file__), "example_5p14") # since working in /tests, must use complete path as file input
 
     # Now from the model we have the essential data: elements, nodes and fields
-    model = Model(path ,[u], [s_u])
+    model = Model(path ,[u], [s_uw])
 
     # Now we add boundary conditions to the model
     model.add_EssentialBc(gamma_u) #.add_LoadBC(gamma_l).add_LoadBC(gamma_dl1).add_LoadBC(gamma_dl2)
